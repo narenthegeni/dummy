c479130b9798b808b47062ad88e3df3f6a381da2
narenthegeni <44600931+narenthegeni@users.noreply.github.com>
2022-06-02 12:28:38 +0530

    Update test.java

--- test.java
+++ test.java
@@ -14,33 +14,39 @@ public class Train {
 		arrayList.add(2);
 		arrayList.add(3);
 		arrayList.add(4);
+		arrayList.add(5);
 		ArrayList<Integer> arrayList1 = new ArrayList<Integer>();
-		arrayList1.add(5);
 		arrayList1.add(6);
 		arrayList1.add(7);
 		arrayList1.add(8);
+		arrayList1.add(9);
+		arrayList1.add(10);
 		ArrayList<Integer> arrayList2 = new ArrayList<Integer>();
-		arrayList2.add(9);
-		arrayList2.add(10);
 		arrayList2.add(11);
 		arrayList2.add(12);
+		arrayList2.add(13);
+		arrayList2.add(14);
+		arrayList2.add(15);
 		ArrayList<Integer> arrayList3 = new ArrayList<Integer>();
-		arrayList3.add(13);
-		arrayList3.add(14);
-		arrayList3.add(15);
 		arrayList3.add(16);
+		arrayList3.add(17);
+		arrayList3.add(18);
+		arrayList3.add(19);
+		arrayList3.add(20);
 		ArrayList<Integer> arrayList4 = new ArrayList<Integer>();
-		arrayList4.add(17);
-		arrayList4.add(18);
-		arrayList4.add(19);
-		arrayList4.add(20);
+		arrayList4.add(21);
+		arrayList4.add(22);
+		arrayList4.add(23);
+		arrayList4.add(24);
+		arrayList4.add(25);
+		
 
 		List<ArrayList<Integer>> input = new ArrayList<ArrayList<Integer>>();
 		input.add(arrayList);
 		input.add(arrayList1);
-//		input.add(arrayList2);
-//		input.add(arrayList3);
-//		input.add(arrayList4);
+		input.add(arrayList2);
+		input.add(arrayList3);
+		input.add(arrayList4);
 		ArrayList<Integer> spiralOrder = train.spiralOrder(input);
 
 		System.out.println(Arrays.toString(spiralOrder.toArray()));
@@ -78,6 +84,9 @@ public class Train {
 				break;
 			}
 			// LEFT
+			if ( endRight == endLeft ) {
+				endLeft--;
+			}
 			for (int i = endRight; i > endLeft; i--) {
 				spiralResult.add(A.get(endDown).get(i));
 			}
@@ -85,6 +94,9 @@ public class Train {
 				break;
 			}
 			// UP
+			if ( endUp == endDown ) {
+				endUp--;
+			}
 			for (int i = endDown; i > endUp; i--) {
 				spiralResult.add(A.get(i).get(endUp));
 			}
@@ -99,6 +111,9 @@ public class Train {
 
 	private ArrayList<Integer> extracted_Down(final List<ArrayList<Integer>> A, ArrayList<Integer> spiralResult,
 			int endRight, int endDown, int endUp) {
+		if ( endUp == endDown ) {
+			endDown++;
+		}
 		for (int i = endUp; i < endDown; i++) {
 			spiralResult.add(A.get(i).get(endRight));
 		}
@@ -107,6 +122,9 @@ public class Train {
 
 	private ArrayList<Integer> extracted(final List<ArrayList<Integer>> A, ArrayList<Integer> spiralResult,
 			int endRight, int endLeft) {
+		if ( endLeft == endRight ) {
+			endRight++;
+		}
 		for (int i = endLeft; i < endRight; i++) {
 			spiralResult.add(A.get(endLeft).get(i));
 		}
