7bf0486fba046c58dd0bd7be198c147b54f33f8e
narenthegeni <44600931+narenthegeni@users.noreply.github.com>
2022-06-03 16:29:48 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,19 +1,6 @@
-
-import java.util.List;
-
 public class Solution {
-	// DO NOT MODIFY THE LIST. IT IS READ ONLY
-	public int repeatedNumber(final List<Integer> A) {
+	public void rotate(ArrayList<ArrayList<Integer>> a) {
 
-		int [] store = new int[A.size()];
-//		System.out.println("Array size" + A.size());
-		for (int i = 0; i < A.size(); i++) {
-			if (store[A.get(i)] == A.get(i)) {
-				return A.get(i);
-			} else {
-				store[A.get(i)] = A.get(i);
-			}
-		}
-		return 1;
+        
 	}
 }
