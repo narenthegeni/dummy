c00b7570ded10927c3d52aae39d4363ccadad77a
narenthegeni <44600931+narenthegeni@users.noreply.github.com>
2022-06-13 22:40:08 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,84 +2,31 @@ public class Solution {
 	public ArrayList<ArrayList<Integer>> rotate(ArrayList<ArrayList<Integer>> a) {
 
 		int row = a.size();
-		int col = a.get(0).size();
-		
+
 		int totalLayer = row > 2 ? row - 2 : row;
-		int direction = 0, m = 0, n = 0, prev = 0, nPointer = -1, mPointer = -1; // 0 - Right, 1 - Down, 2 - Left, 3 - Up
-		for (int loop = 1; loop <= totalLayer; loop++) {
-			for (int rowRotate = 1; rowRotate <= row - (nPointer + 2); rowRotate++) {
-				int cur = a.get(m).get(n);
-				for (int i = 0; i <= ((row - (nPointer + 1))* 4) - 4; i++) {
+		int m = 0, n = 0, prev = 0; // 0 - Right, 1 - Down, 2 - Left, 3 - Up
+		for (int loop = 0; loop < totalLayer; loop++) {
+			int layer = row - 1 - loop;
+			int limitLayer = layer - m;
+			for (int i = 0; i < limitLayer; i++) {
 
-					if (row <= 1) {
-						break;
-					}
-					switch (direction) {
+				int cur = a.get(m).get(n + i);
+				prev = a.get(m + i).get(layer);
+				a.get(m + i).set(layer, cur);
+				cur = prev;
 
-					// Right
-					case 0:
-						n++;
-						if (n >= col) {
-							direction++;
-							n--;
-						} else {
-							prev = a.get(m).get(n);
-							a.get(m).set(n, cur);
-							cur = prev;
-							break;
-						}
+				prev = a.get(layer).get(layer - i);
+				a.get(layer).set(layer - i, cur);
+				cur = prev;
 
-						// Down
-					case 1:
-						m++;
-						if (m >= row) {
-							direction++;
-							m--;
-						} else {
-							prev = a.get(m).get(n);
-							a.get(m).set(n, cur);
-							cur = prev;
-							break;
-						}
+				prev = a.get(layer - i).get(n);
+				a.get(layer - i).set(n, cur);
+				cur = prev;
 
-						// Left
-					case 2:
-						n--;
-						if (n <= nPointer) {
-							direction++;
-							n++;
-						} else {
-							prev = a.get(m).get(n);
-							a.get(m).set(n, cur);
-							cur = prev;
-							break;
-						}
-
-						// Up
-					case 3:
-						m--;
-						if (m <= mPointer) {
-							direction = 0;
-							m++;
-						} else {
-							prev = a.get(m).get(n);
-							a.get(m).set(n, cur);
-							cur = prev;
-							break;
-						}
-
-					default:
-						break;
-
-					}
-				}
+				a.get(m).set(n + i, cur);
 			}
-			row --;
-			col --;
 			m++;
 			n++;
-			mPointer++;
-			nPointer++;
 		}
 
 		return a;
