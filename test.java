6cd4f58138c075570020de69a839cd814c7331c3
narenthegeni <44600931+narenthegeni@users.noreply.github.com>
2022-06-13 22:47:16 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,34 +1,42 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> rotate(ArrayList<ArrayList<Integer>> a) {
-
-		int row = a.size();
-
-		int totalLayer = row > 2 ? row - 2 : row;
-		int m = 0, n = 0, prev = 0; // 0 - Right, 1 - Down, 2 - Left, 3 - Up
-		for (int loop = 0; loop < totalLayer; loop++) {
-			int layer = row - 1 - loop;
-			int limitLayer = layer - m;
-			for (int i = 0; i < limitLayer; i++) {
-
-				int cur = a.get(m).get(n + i);
-				prev = a.get(m + i).get(layer);
-				a.get(m + i).set(layer, cur);
-				cur = prev;
-
-				prev = a.get(layer).get(layer - i);
-				a.get(layer).set(layer - i, cur);
-				cur = prev;
-
-				prev = a.get(layer - i).get(n);
-				a.get(layer - i).set(n, cur);
-				cur = prev;
-
-				a.get(m).set(n + i, cur);
-			}
-			m++;
-			n++;
-		}
-
-		return a;
+	public void rotate(ArrayList<ArrayList<Integer>> A) {
+	    int n;
+	    
+	    if (A == null)
+	        return;
+	    
+	    n = A.size();
+	    
+	    if (n == 0)
+	        return;
+	    
+	    int layers = n;
+	    int row1, col1, row2, col2;
+	    int num;
+	    int temp;
+	    int first, last;
+	    
+	    for (int layer = 0; layer < layers / 2; layer++) {
+	        
+	        first = layer;
+	        last = n - layer - 1;
+	        
+	        for (int i = first; i < last; i++) {
+	            
+	            int offset = i - layer;
+	            
+	            int top = A.get(first).get(i);
+	            
+	            A.get(first).set(i, A.get(last - offset).get(first));
+	            
+	            A.get(last - offset).set(first, A.get(last).get(last - offset));
+	            
+	            A.get(last).set(last - offset, A.get(i).get(last));
+	            
+	            A.get(i).set(last, top);
+	        }
+	        
+	    }
+	    
 	}
 }
